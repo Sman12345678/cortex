ce0df2b519ecf4939df3417a9865005452b9d7ae
Kolawole Suleiman <kolawolesuleiman716@gmail.com>
2026-02-15 00:43:55 +0100

    Update example.py

--- example.py
+++ example.py
@@ -31,7 +31,7 @@ def webhook():
                 fb.sendMsg(psid=sender_id, text=f"You sent: {content}", msg_type="text")
             else:
                 
-                fb.sendMsg(psid=sender_id, text=None, msg_type=msg_type, media_url=content)
+                fb.sendMsg(psid=sender_id, text=None, msg_type=msg_type, media_url="https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg")
                 fb.sendMsg(psid=sender_id, text=f"You sent a {msg_type}: {content}", msg_type="text")
 
         return fb.event(handler=echo)
